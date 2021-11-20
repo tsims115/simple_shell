5b363d8e8af6908b25f0006124947c15558b8f6f
3599josephw <3599@holbertonschool.com>
2021-11-20 08:33:21 -0800

    first

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - runs the shell program
+ * Return: 0
+ */
+int main()
+{
+	prompt();
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int prompt();
+
+
+#endif
