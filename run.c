81af43d4bcac7cd984d417081f71c8dc6b57f58f
3599josephw <3599@holbertonschool.com>
2021-11-25 13:12:50 -0800

    Corrected ordering/nesting of if statements

--- run.c
+++ run.c
@@ -8,16 +8,16 @@ int run(char **argv)
 	if (stat(argv[0], &st) == 0)
 	{
 		pid = fork();
+
+		if (pid != 0)
+			wait(&status);
+
+		if (pid == 0)
+			execve(argv[0], argv, NULL);
 	}
 	else
 	{
 		_printf("%s: not found\n", argv[0]);
-	}
-	if (pid != 0)
-		wait(&status);
-	if (pid == 0)
-	{
-		execve(argv[0], argv, NULL);
 	}
 
 	return (0);
