f6d7bdcbbe3cb7aeb90bba73b810b8d5e418ef4a
3599josephw <3599@holbertonschool.com>
2021-11-26 10:35:07 -0800

    while -> do while: handles EOF and extra arguments

--- simple_shell.c
+++ simple_shell.c
@@ -11,14 +11,23 @@ int main(void)
 	char s[100];
 	size_t n;
 	char *command = NULL;
+	int length;
 
 	if (str == NULL)
 	{
 		return (-1);
 	}
-	_printf("%s$ ", getcwd(s, 100));
-	while (getline(&str, &bufsize, stdin) != -1)
-	{
+	do {
+		_printf("%s$ ", getcwd(s, 100));
+
+		length = getline(&str, &bufsize, stdin);
+		if (length == EOF)
+		{
+			_putchar('\n');
+			free(str);
+			exit(-1);
+		}
+
 		if (strcmp(str, "exit\n") == 0)
 			free(str), exit(0);
 
@@ -28,11 +37,8 @@ int main(void)
 		if (strcmp(argv[0], "cd") == 0)
 			chdir(argv[1]);
 		else
-		{
 			run(argv);
-		}
-		_printf("%s$ ", getcwd(s, 100));
-	}
+	} while (length != -1);
 	free(str);
 	return (0);
 }
